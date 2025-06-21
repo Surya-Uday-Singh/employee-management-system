0d27ecc4a3161198843d9d130aeb7351e9d30ba5
Surya Uday Singh <suryauday915@gmail.com>
2025-06-21 01:57:18 -0400

    Updates and added DockerFile

--- employee-service/src/main/java/com/em/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/em/employeeservice/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.em.employeeservice.dto.EmployeeRequestDTO;
 import com.em.employeeservice.dto.EmployeeResponseDTO;
 import com.em.employeeservice.dto.validators.CreateEmployeeValidationGroup;
 import com.em.employeeservice.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/employees")
+@Tag(name = "Employee", description = "API that manages Employee in an organization")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
@@ -23,24 +26,28 @@ public class EmployeeController {
     }
 
     @GetMapping
+    @Operation(summary = "Get Employees")
     public ResponseEntity<List<EmployeeResponseDTO>> getEmployees() {
         List<EmployeeResponseDTO> employees = employeeService.getEmployees();
         return ResponseEntity.ok().body(employees);
     }
 
     @PostMapping
+    @Operation(summary = "Creates a new Employee")
     public ResponseEntity<EmployeeResponseDTO> createEmployee(@Validated({Default.class, CreateEmployeeValidationGroup.class}) @RequestBody EmployeeRequestDTO employeeRequestDTO) {
         EmployeeResponseDTO employeeResponseDTO = employeeService.createEmployee(employeeRequestDTO);
         return ResponseEntity.ok().body(employeeResponseDTO);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Updates an Employee")
     public ResponseEntity<EmployeeResponseDTO> updateEmployee(@PathVariable UUID id, @Validated({Default.class}) @RequestBody EmployeeRequestDTO employeeRequestDTO) {
         EmployeeResponseDTO employeeResponseDTO = employeeService.updateEmployee(id, employeeRequestDTO);
         return ResponseEntity.ok().body(employeeResponseDTO);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes an Employee")
     public ResponseEntity<Void> deleteEmployee(@PathVariable UUID id) {
         employeeService.deleteEmployee(id);
         return ResponseEntity.noContent().build();
